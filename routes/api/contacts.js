d3fe020a00254a358d85f2fe602dfa8e4f026029
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2023-08-31 23:03:48 +0200

    -

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,4 +1,9 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
+const passport = require('passport')
+const User = require('../../schemas/user.model')
+require('dotenv').config()
+const secret = process.env.SECRET
 const Joi = require('joi');
 const {
     listContacts,
@@ -20,6 +25,88 @@ const putContactSchema = Joi.object({
     email: Joi.string().email(),
     phone: Joi.string(),
 });
+
+const auth = (req, res, next) => {
+    passport.authenticate('jwt', {session: false}, (err, user) => {
+        if (!user || err) {
+            return res.status(401).json({
+                status: 'error',
+                code: 401,
+                message: 'Unauthorized',
+                data: 'Unauthorized',
+            })
+        }
+        req.user = user
+        next()
+    })(req, res, next)
+}
+
+router.post('/login', async (req, res, next) => {
+    const {email, password} = req.body
+    const user = await User.findOne({email})
+
+    if (!user || !user.validPassword(password)) {
+        return res.status(400).json({
+            status: 'error',
+            code: 400,
+            message: 'Incorrect login or password',
+            data: 'Bad request',
+        })
+    }
+
+    const payload = {
+        id: user.id,
+        username: user.username,
+    }
+
+    const token = jwt.sign(payload, secret, {expiresIn: '1h'})
+    res.json({
+        status: 'success',
+        code: 200,
+        data: {
+            token,
+        },
+    })
+
+})
+
+router.post('/registration', async (req, res, next) => {
+    const {username, email, password} = req.body
+    const user = await User.findOne({email})
+    if (user) {
+        return res.status(409).json({
+            status: 'error',
+            code: 409,
+            message: 'Email is already in use',
+            data: 'Conflict',
+        })
+    }
+    try {
+        const newUser = new User({username, email})
+        newUser.setPassword(password)
+        await newUser.save()
+        res.status(201).json({
+            status: 'success',
+            code: 201,
+            data: {
+                message: 'Registration successful',
+            },
+        })
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.get('/list', auth, (req, res, next) => {
+    const {username} = req.user
+    res.json({
+        status: 'success',
+        code: 200,
+        data: {
+            message: `Authorization was successful: ${username}`,
+        },
+    })
+})
 
 router.get('/', async (req, res, next) => {
     try {
@@ -160,11 +247,19 @@ router.patch('/:contactId/favorite', async (req, res, next) => {
                 });
             } else {
                 await updateStatusContact(contactId, body.favorite);
-                res.status(200).json({
-                    status: 'success',
-                    code: 200,
-                    message: 'Contact added to favorites'
-                })
+                if (body.favorite.valueOf() === true) {
+                    res.status(200).json({
+                        status: 'success',
+                        code: 200,
+                        message: 'Contact added to favorites'
+                    })
+                } else if (body.favorite.valueOf() === false) {
+                    res.status(200).json({
+                        status: 'success',
+                        code: 200,
+                        message: 'Contact removed from favorites'
+                    })
+                }
             }
         }
     } catch (error) {
